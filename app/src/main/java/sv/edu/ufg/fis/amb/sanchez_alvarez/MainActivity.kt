8f3d4af00c48c7accfd244d55e2e1204418460c0
foselin <foselinalvarez@gmail.com>
2024-04-13 00:46:18 -0600

    Primer commit

---
+++ app/src/main/java/sv/edu/ufg/fis/amb/sanchez_alvarez/MainActivity.kt
@@ -0,0 +1,80 @@
+package sv.edu.ufg.fis.amb.sanchez_alvarez
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var navegacion : BottomNavigationView
+
+   private val opcionMenuSeleccionada = BottomNavigationView.OnNavigationItemSelectedListener{item ->
+        when(item.itemId){
+            R.id.fragmento1->{
+                supportFragmentManager.commit {
+                    replace<primer_fragmento>(R.id.fragment_container_view)
+                    setReorderingAllowed(true)
+                    addToBackStack("replacement")
+                }
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.fragmento2->{
+                supportFragmentManager.commit {
+                    replace<segundo_fragmento>(R.id.fragment_container_view)
+                    setReorderingAllowed(true)
+                    addToBackStack("replacement")
+                }
+                return@OnNavigationItemSelectedListener true
+            }
+
+
+            R.id.fragmento3->{
+                supportFragmentManager.commit {
+                    replace<tercer_fragmento>(R.id.fragment_container_view)
+                    setReorderingAllowed(true)
+                    addToBackStack("replacement")
+                }
+                return@OnNavigationItemSelectedListener true
+            }
+
+            R.id.fragmento4->{
+                supportFragmentManager.commit {
+                    replace<cuarto_fragmento>(R.id.fragment_container_view)
+                    setReorderingAllowed(true)
+                    addToBackStack("replacement")
+                }
+                return@OnNavigationItemSelectedListener true
+            }
+
+        }
+        false
+    }
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager.commit {
+            replace<primer_fragmento>(R.id.fragment_container_view)
+            setReorderingAllowed(true)
+            addToBackStack("replacement")
+        }
+
+        navegacion =findViewById(R.id.nav_menu)
+        navegacion.setOnNavigationItemSelectedListener(opcionMenuSeleccionada)
+    }
+
+
+
+
+}
